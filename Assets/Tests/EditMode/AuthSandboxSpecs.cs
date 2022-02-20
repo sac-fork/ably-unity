fe14c6f46253a513f4c95233c022dd85b5ef95ab
sacOO7 <sachinshinde7676@gmail.com>
2022-02-21 00:42:55 +0530

    Removed folder based ably asset versioning, reverted back to plain asset name

--- Assets/Tests/EditMode/AuthSandboxSpecs.cs
+++ Assets/Tests/EditMode/AuthSandboxSpecs.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Assets.Tests.AblySandbox;
 using Cysharp.Threading.Tasks;
 using FluentAssertions;
 using IO.Ably;
