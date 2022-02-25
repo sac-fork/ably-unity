828051dd82c2855e83abcc19fae711536067409a
sacOO7 <sachinshinde7676@gmail.com>
2022-02-25 10:12:00 +0530

    Updated namespce braces for AblyConnection

--- Assets/Ably/Examples/AblyConnection.cs
+++ Assets/Ably/Examples/AblyConnection.cs
@@ -3,66 +3,67 @@ using IO.Ably.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Assets.Ably.Examples;
-
-public class AblyConnection : MonoBehaviour
+namespace Assets.Ably.Examples
 {
-    AblyRealtime ably;
-    public Text errorText;
-    public Image image;
-
-    private Color color;
-    private string text;
-
-    void Start()
+    public class AblyConnection : MonoBehaviour
     {
-        image.color = Color.cyan;
-    }
+        AblyRealtime ably;
+        public Text errorText;
+        public Image image;
 
-    void Connect()
-    {
-        text = "";
-        color = Color.yellow;
+        private Color color;
+        private string text;
 
-        var options = new ClientOptions();
-        options.Key = "";
-        // this will disable the library trying to subscribe to network state notifications
-        options.AutomaticNetworkStateMonitoring = false;
-        // you can also turn on if you need to update the UI directly from channel subscription handlers
-        options.CaptureCurrentSynchronizationContext = true;
-
-        ably = new AblyRealtime(options);
-        ably.Connection.On(ConnectionEvent.Connected, args =>
+        void Start()
         {
-            Debug.Log("Connected to Ably!");
-            color = Color.green;
-            text = "Connected to Ably!";
-        });
-
-        var channel = ably.Channels.Get("notifications");
-        channel.Subscribe(message =>
-        {
-            Debug.Log("Received a greeting message in realtime: " + message.Data);
-            color = Color.blue;
-            text = "Received a greeting message in realtime: " + message.Data;
-        });
-
-    }
-
-    void Update()
-    {
-        errorText.text = text;
-        image.color = color;
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Connect();
+            image.color = Color.cyan;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        void Connect()
         {
+            text = "";
+            color = Color.yellow;
+
+            var options = new ClientOptions();
+            options.Key = "";
+            // this will disable the library trying to subscribe to network state notifications
+            options.AutomaticNetworkStateMonitoring = false;
+            // you can also turn on if you need to update the UI directly from channel subscription handlers
+            options.CaptureCurrentSynchronizationContext = true;
+
+            ably = new AblyRealtime(options);
+            ably.Connection.On(ConnectionEvent.Connected, args =>
+            {
+                Debug.Log("Connected to Ably!");
+                color = Color.green;
+                text = "Connected to Ably!";
+            });
+
             var channel = ably.Channels.Get("notifications");
-            channel.Publish("greeting", "hello!");
+            channel.Subscribe(message =>
+            {
+                Debug.Log("Received a greeting message in realtime: " + message.Data);
+                color = Color.blue;
+                text = "Received a greeting message in realtime: " + message.Data;
+            });
+
+        }
+
+        void Update()
+        {
+            errorText.text = text;
+            image.color = color;
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Connect();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                var channel = ably.Channels.Get("notifications");
+                channel.Publish("greeting", "hello!");
+            }
         }
     }
 }
